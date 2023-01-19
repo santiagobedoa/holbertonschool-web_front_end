7ed4ee57c076afe0a47b787be2b6aaa4e9abdcc8
santiagobedoa <santiagobedoa@gmail.com>
2023-01-19 13:07:50 -0500

    Thu Jan 19 13:07:50 -05 2023

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,8 @@
+function welcomeMessage(fullName) {
+  return () => {
+    alert(`Welcome ${fullName}`);
+  };
+}
+let guillaume = welcomeMessage("Guillaume");
+let alex = welcomeMessage("Alex");
+let fred = welcomeMessage("Fred");
