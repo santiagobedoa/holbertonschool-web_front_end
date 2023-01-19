a8ca654ace213caddb8555825b28a34228861e6b
santiagobedoa <santiagobedoa@gmail.com>
2023-01-19 17:55:52 -0500

    Thu Jan 19 17:55:52 -05 2023

---
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -0,0 +1,24 @@
+function countPrimeNumbers() {
+  let notPrimes = 0;
+  for (let number = 2; number <= 200; number++) {
+    for (let j = 2; j < number; j++) {
+      if (number % j == 0) {
+        notPrimes++;
+        break;
+      }
+    }
+  }
+  return 198 - notPrimes;
+}
+
+let start = performance.now();
+for (let i = 0; i < 100; i++) {
+  countPrimeNumbers();
+}
+let end = performance.now();
+
+console.log(
+  `Execution time of printing countPrimeNumbers was ${
+    end - start
+  } milliseconds.`
+);
